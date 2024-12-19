b47f7f1211616cd9eebc3d5c3a457315c6266087
Xnuk Shuman <admin@xnu.kr>
2024-12-19 17:55:23 +0900

    dummy stuffs

--- src/main/kotlin/KeyboardTester.kt
+++ src/main/kotlin/KeyboardTester.kt
@@ -1,26 +1,35 @@
 package kr.xnu.keyboard.semen
 
 import android.os.Bundle
+import android.view.View
+import android.view.ViewGroup
 import android.widget.EditText
 import android.widget.RelativeLayout
 import android.widget.Toast
 import androidx.activity.ComponentActivity
 
-class KeyboardTester : ComponentActivity() {
+class KeyboardTester : ComponentActivity(), IcCondom {
 	private var toastBefore: Toast? = null
+	private var state: EngineDirectState? = null
+
+	private var keyboard: View? = null
+	private val keyboardLayoutParams = RelativeLayout.LayoutParams(
+		RelativeLayout.LayoutParams.MATCH_PARENT,
+		RelativeLayout.LayoutParams.WRAP_CONTENT
+	).apply {
+		addRule(RelativeLayout.ALIGN_PARENT_BOTTOM)
+	}
+
 	override fun onCreate(savedInstanceState: Bundle?) {
 		super.onCreate(savedInstanceState)
 
 		val parent = RelativeLayout(this)
+		val state = EngineDirectState(defaultKeyboard.keyboard, this)
+		this.state = state
 
-		val k = keyboardView(this, defaultKeyboard.keyboard.layers[0]) {
-			toastBefore?.cancel()
-			val t = Toast.makeText(this, "$it", Toast.LENGTH_SHORT)
-			t.show()
-			toastBefore = t
-
-		}
+		val k = state.render(this)
 		k.id = R.id.keyboard
+		keyboard = k
 
 		parent.addView(
 			EditText(this).apply { hint = "Dick" },
@@ -32,15 +41,7 @@ class KeyboardTester : ComponentActivity() {
 			}
 		)
 
-		parent.addView(
-			k,
-			RelativeLayout.LayoutParams(
-				RelativeLayout.LayoutParams.MATCH_PARENT,
-				RelativeLayout.LayoutParams.WRAP_CONTENT
-			).apply {
-				addRule(RelativeLayout.ALIGN_PARENT_BOTTOM)
-			}
-		)
+		parent.addView(k, keyboardLayoutParams)
 
 		setContentView(
 			parent,
@@ -48,7 +49,54 @@ class KeyboardTester : ComponentActivity() {
 				RelativeLayout.LayoutParams.MATCH_PARENT,
 				RelativeLayout.LayoutParams.MATCH_PARENT
 			)
-
 		)
+	}
+
+	override fun commitText(str: String) {
+		toastBefore?.cancel()
+		toastBefore =
+			Toast.makeText(this, "commit: $str", Toast.LENGTH_SHORT)
+				.apply { show() }
+	}
+
+	override fun preeditText(str: String) {
+		toastBefore?.cancel()
+		toastBefore =
+			Toast.makeText(this, "preedit: $str", Toast.LENGTH_SHORT)
+				.apply { show() }
+	}
+
+	override fun deleteSelectedText(): Boolean {
+		toastBefore?.cancel()
+		toastBefore =
+			Toast.makeText(this, "deleteSelectedText", Toast.LENGTH_SHORT)
+				.apply { show() }
+		return true
+	}
+
+	override fun deleteOneBefore() {
+		toastBefore?.cancel()
+		toastBefore =
+			Toast.makeText(this, "deleteOneBefore", Toast.LENGTH_SHORT)
+				.apply { show() }
+	}
+
+	override fun onEnter(): Boolean {
+		toastBefore?.cancel()
+		toastBefore =
+			Toast.makeText(this, "Enter", Toast.LENGTH_SHORT).apply { show() }
+		return true
+	}
+
+	override fun onRenderListener() {
+		val k = keyboard
+		val newK = state?.render(this)
+		val parent = k?.parent as? ViewGroup
+		if (k != null && parent != null && newK != null) {
+			val index = parent.indexOfChild(k)
+			parent.removeView(k)
+			newK.id = R.id.keyboard
+			parent.addView(newK, index, keyboardLayoutParams)
+		}
 	}
 }
