e1bb768b2f44e75dce8322866d7990f297ac6de1
Xnuk Shuman <admin@xnu.kr>
2024-12-19 15:18:38 +0900

    나는 relativelayout을 정복했다 (아마도)

--- src/main/kotlin/KeyboardTester.kt
+++ src/main/kotlin/KeyboardTester.kt
@@ -1,8 +1,8 @@
 package kr.xnu.keyboard.semen
 
 import android.os.Bundle
-import android.view.ViewGroup
-import android.widget.GridLayout
+import android.widget.EditText
+import android.widget.RelativeLayout
 import android.widget.Toast
 import androidx.activity.ComponentActivity
 
@@ -10,21 +10,45 @@ class KeyboardTester : ComponentActivity() {
 	private var toastBefore: Toast? = null
 	override fun onCreate(savedInstanceState: Bundle?) {
 		super.onCreate(savedInstanceState)
+
+		val parent = RelativeLayout(this)
+
 		val k = keyboardView(this, defaultKeyboard.keyboard.layers[0]) {
 			toastBefore?.cancel()
 			val t = Toast.makeText(this, "$it", Toast.LENGTH_SHORT)
 			t.show()
 			toastBefore = t
+
 		}
+		k.id = R.id.keyboard
+
+		parent.addView(
+			EditText(this).apply { hint = "Dick" },
+			RelativeLayout.LayoutParams(
+				RelativeLayout.LayoutParams.MATCH_PARENT,
+				RelativeLayout.LayoutParams.WRAP_CONTENT
+			).apply {
+				addRule(RelativeLayout.ABOVE, R.id.keyboard)
+			}
+		)
+
+		parent.addView(
+			k,
+			RelativeLayout.LayoutParams(
+				RelativeLayout.LayoutParams.MATCH_PARENT,
+				RelativeLayout.LayoutParams.WRAP_CONTENT
+			).apply {
+				addRule(RelativeLayout.ALIGN_PARENT_BOTTOM)
+			}
+		)
 
 		setContentView(
-			k,
-			GridLayout.LayoutParams(
-				ViewGroup.LayoutParams(
-					ViewGroup.LayoutParams.MATCH_PARENT,
-					ViewGroup.LayoutParams.WRAP_CONTENT
-				)
+			parent,
+			RelativeLayout.LayoutParams(
+				RelativeLayout.LayoutParams.MATCH_PARENT,
+				RelativeLayout.LayoutParams.MATCH_PARENT
 			)
+
 		)
 	}
 }
